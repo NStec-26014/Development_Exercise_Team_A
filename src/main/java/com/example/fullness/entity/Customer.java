8c3c282edf1898e33b50441495ae59e2ef928e57
NStec-26014 <nstec26_014@fullness.email>
2026-08-05 13:49:15 +0900

    コンフリクト完全解消

---
+++ src/main/java/com/example/fullness/entity/Customer.java
@@ -0,0 +1,13 @@
+package com.example.fullness.entity;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class Customer implements Serializable{
+    private Serial id;
+    private String name;
+    
+
+}
--- src/main/java/com/example/fullness/entity/Orders.java
+++ src/main/java/com/example/fullness/entity/Orders.java
@@ -1,9 +1,11 @@
 package com.example.fullness.entity;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class Orders{
+public class Orders implements Serializable{
     private Serial id;
     private Timestamp orderData;
     private Integer amountTotal;
