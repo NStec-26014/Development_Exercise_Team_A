1a2720179c80d995302c6830ac9d3853dba21861
NStec-26014 <nstec26_014@fullness.email>
2026-08-24 08:55:23 +0900

    Revert "teamA_UC012"

--- src/main/java/com/example/fullness/stationary/service/ProductService.java
+++ src/main/java/com/example/fullness/stationary/service/ProductService.java
@@ -42,16 +42,9 @@ public class ProductService {
         categoryMapper.insert(category);
     }
 
-    // 商品削除
+    // 商品削除（論理削除）
     public void deleteProduct(Long id) {
         productMapper.deleteById(id);
-    }
-
-    // 商品修正
-    public void editProduct(Product product) {
-        productMapper.edit(product);
-
-        productMapper.updateStock(product.getId(), product.getQuantity());
     }
 
     // 商品詳細取得
